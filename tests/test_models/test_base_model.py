301db4a3139a86ca0ea6e30711382af9f70c9f1b
coder-akash <>
2023-03-12 22:44:01 -0700

    task done

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,45 +1,46 @@
 #!/usr/bin/python3
-""" """
+"""H"""
 from models.base_model import BaseModel
 import unittest
-from datetime import datetime
+import datetime
+from uuid import UUID
 import json
 import os
 
 
 class test_basemodel(unittest.TestCase):
-    """ """
+    """H"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """base_model"""
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """ """
+        """base_model"""
         pass
 
     def tearDown(self):
         try:
             os.remove('file.json')
-        except:
+        except Exception:
             pass
 
     def test_default(self):
-        """ """
+        """base_model"""
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ """
+        """base_model"""
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ """
+        """base_model"""
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -56,37 +57,50 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """ """
+        """base_model"""
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """ """
+        """base_model"""
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """ """
+        """base_model"""
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
+    def test_kwargs_one(self):
+        """base_model"""
+        n = {'Name': 'test'}
+        try:
+            with self.assertRaises(KeyError):
+                new = self.value(**n)
+        except Exception:
+            pass
+
     def test_id(self):
-        """ """
+        """base_model"""
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """ """
+        """base_model"""
         new = self.value()
-        self.assertEqual(type(new.created_at), datetime)
+        self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """ """
+        """base_model"""
         new = self.value()
-        self.assertEqual(type(new.updated_at), datetime)
+        self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
         new = BaseModel(**n)
         self.assertFalse(new.created_at == new.updated_at)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-""" """
+"""City"""
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
 
 class test_City(test_basemodel):
-    """ """
+    """City"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """City"""
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
-    """
     def test_state_id(self):
-        """ """
+        """State_id"""
         new = self.value()
-        self.assertEqual(type(new.state_id), str)
+        self.assertEqual((new.state_id), None)
 
     def test_name(self):
-        """ """
+        """test_name"""
         new = self.value()
-        self.assertEqual(type(new.name), str)
-    """
+        self.assertEqual((new.name), None)
